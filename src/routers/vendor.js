9674b6ce23299110d7ab553e0e7f6366c8610836
Travis Butler <trbutler@ferrandinoandson.com>
2019-08-23 15:23:37 -0400

    added ability to remove vendor by ID

--- src/routers/vendor.js
+++ src/routers/vendor.js
@@ -3,6 +3,7 @@ const Vendor = require("../models/vendor");
 
 const router = new express.Router();
 
+// Adds a new vendor
 router.post("/new-vendor", async (req, res) => {
 	const vendor = new Vendor(req.body);
 
@@ -13,15 +14,29 @@ router.post("/new-vendor", async (req, res) => {
 		res.status(400).send(e);
 	}
 });
-
-router.get("/vendors", (req, res) => {
-	Vendor.find({})
+// Get a list of all vendors
+router.get("/vendors", async (req, res) => {
+	await Vendor.find({})
 		.then(vendors => {
 			res.send(vendors);
 		})
 		.catch(e => {
 			res.status(500).send(e);
 		});
+});
+// Select a vendor by _id and delete
+router.delete("/vendors/:id", async (req, res) => {
+	try {
+		const vendor = await Vendor.findOneAndDelete({ _id: req.params.id });
+
+		if (!vendor) {
+			res.status(404).send();
+		}
+
+		res.send(vendor);
+	} catch (e) {
+		res.status(500).send();
+	}
 });
 
 module.exports = router;
