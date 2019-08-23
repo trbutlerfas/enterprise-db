c368b8b5f1c8228dea9570493f1ef271b9394bdf
Travis Butler <trbutler@ferrandinoandson.com>
2019-08-23 12:50:37 -0400

    removed unused packages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-require('./db/mongoose');
-const vendorRouter = require('./routers/vendor');
+const express = require("express");
+require("./db/mongoose");
+const vendorRouter = require("./routers/vendor");
 
 const app = express();
 
--- src/routers/vendor.js
+++ src/routers/vendor.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const Vendor = require('../models/vendor');
+const express = require("express");
+const Vendor = require("../models/vendor");
 
 const router = new express.Router();
 
-router.post('/new-vendor', async (req, res) => {
+router.post("/new-vendor", async (req, res) => {
 	const vendor = new Vendor(req.body);
 
 	try {
@@ -12,6 +12,16 @@ router.post('/new-vendor', async (req, res) => {
 	} catch (e) {
 		res.status(400).send(e);
 	}
+});
+
+router.get("/vendors", (req, res) => {
+	Vendor.find({})
+		.then(vendors => {
+			res.send(vendors);
+		})
+		.catch(e => {
+			res.status(500).send(e);
+		});
 });
 
 module.exports = router;
