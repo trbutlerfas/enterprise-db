017cc20da421c9bb73ec012555e1f2d82cf1c351
Travis Butler <trbutler@lpt-pa-mac003.local>
2019-09-11 13:50:14 -0400

    require path

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const vendorRouter = require("./routers/vendor");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
