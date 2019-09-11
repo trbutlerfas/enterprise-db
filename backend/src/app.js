2d0e75a266ab958d7acb0dc31701f291c2c67ad4
Travis Butler <trbutler@lpt-pa-mac003.local>
2019-09-11 13:43:09 -0400

    app.use change

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/vendors", vendorRouter);
+app.use("/", express.static(path.join(__dirname, "../frontend/build")));
 
 module.exports = app;
