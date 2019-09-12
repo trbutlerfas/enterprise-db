87fad28e238b87d03e73b7dcb8d7b33dacccc259
Travis Butler <trbutler@lpt-pa-mac003.local>
2019-09-12 09:26:24 -0400

    changed frontend path in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use("/", express.static(path.join(__dirname, "../frontend/build")));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 /*React root*/
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "../client/build/index.html"));
+	res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
 });
 
 module.exports = app;
