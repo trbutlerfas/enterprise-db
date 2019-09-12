78e406ebb60411a268c1f35d12591ec594fe1e4b
Travis Butler <trbutler@lpt-pa-mac003.local>
2019-09-12 09:32:37 -0400

    modified backend paths

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/vendors", vendorRouter);
-app.use("/", express.static(path.join(__dirname, "../frontend/build")));
 
 /*Adds the react production build to serve react requests*/
 app.use(express.static(path.join(__dirname, "../frontend/build")));
